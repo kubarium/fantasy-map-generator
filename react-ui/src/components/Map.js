f8dd12bb23792d1b8331b0fbd1ca83885b7ab60a
kubarium <iam@denizkumsal.com>
2017-04-07 16:38:53 -0400

    fixes images and tiles display

--- react-ui/src/components/Map.js
+++ react-ui/src/components/Map.js
@@ -1,25 +1,31 @@
-import '../styles/Map.css'
 import '../styles/Tile.css'
 
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 import className from 'classnames'
 
 class Map extends Component {
     render() {
         const tiles = [...Array(64).keys()].map((x, x_index) => [...Array(64).keys()].map((y, y_index) => (
-            <li className={ className({
-                    tile: true,
-                    [`tile-${x_index}-${y_index}`]: true
-                }) } key={ `${x_index}-${y_index}` }>
-              { `${x_index}-${y_index}` }
+            <li
+                className={className({
+                tile: true,
+                [`tile-${x_index}-${y_index}`]: true
+            })}
+                key={`${x_index}-${y_index}`}>
+                <span className="tile-label">{`${x_index}-${y_index}`}</span>
+                <div className="layers">
+                <img src={`/pieces/island-${x_index}.png`} width="64" height="64" />
+
+                </div>
             </li>
+
         )))
         return (
             <div className="map">
-              <ul>
-                { tiles }
-              </ul>
+                <ul>
+                    {tiles}
+                </ul>
             </div>
         )
     }

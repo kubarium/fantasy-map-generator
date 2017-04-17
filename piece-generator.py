3a9285162d053bfc0755a058aa9fdea65cb4523e
kubarium <iam@denizkumsal.com>
2017-04-17 17:46:54 -0400

    fixes the bottom line finish

--- piece-generator.py
+++ piece-generator.py
@@ -1,5 +1,4 @@
 import numpy as np
-import random
 
 dimension = 16
 
@@ -7,7 +6,7 @@ export = np.full([dimension,dimension],0)
 
 source = np.random.rand(dimension,dimension)
 
-start_interval = 2
+start_interval = 1
 start_index = int(dimension /4 * start_interval)
 
 def horizontal():
@@ -17,15 +16,26 @@ def horizontal():
             export[i][start_index] = 1
             continue
 
+        if i == len(source) - 1 :
+            if start_index in (0,1):
+                start_index = 0
+            if start_index in (2,6,10,14,15):
+                start_index = 15
+            if start_index in (3,4,5):
+                start_index = 4
+            if start_index in (7,8,9):
+                start_index = 8
+            if start_index in (11,12,13):
+                start_index = 12
+            
+            export[i][start_index] = 1
+            continue
+
         sorted_values = sorted([(start_index-1, source[i][start_index-1]), (start_index, source[i][start_index]), (start_index+1, source[i][start_index+1])], key=lambda tup: tup[1])
 
         #the middle value (1) and 0th index always gives the median index and the next start index to trickle down
         start_index = sorted_values[1][0]
         export[i][start_index] = 1
-
-        if i == len(source) - 1 :
-
-
 
 horizontal()
 print(export)
